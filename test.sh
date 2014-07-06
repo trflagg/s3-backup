8828c3f46943b593974db8e13b23e7c2845108b4
Taylor Flagg <trflagg@gmail.com>
2014-07-05 21:51:02 -0400

    Make tests point to trflagg/s3-backup

--- test.sh
+++ test.sh
@@ -14,42 +14,42 @@ then
 fi
 
 pushd backup
-docker build -t digit/s3-backup .
+sudo docker build -t trflagg/s3-backup .
 popd
 
 pushd restore
-docker build -t digit/s3-restore .
+sudo docker build -t trflagg/s3-restore .
 popd
 
 function cleanup {
   echo "Cleaning up"
-  ids_to_kill=$(docker ps -a | grep s3-backup-test | awk '{ print $1 }')
+  ids_to_kill=$(sudo docker ps -a | grep s3-backup-test | awk '{ print $1 }')
   for id in $ids_to_kill
   do
-    docker stop $id || true
-    docker rm $id
+    sudo docker stop $id || true
+    sudo docker rm $id
   done
 }
 
 cleanup
 
 # create a data container
-docker run --name s3-backup-test-data-original --volume /var/s3-backup busybox true
+sudo docker run --name s3-backup-test-data-original --volume /var/s3-backup busybox true
 
 # write some data to the container
-docker run --name s3-backup-test-writer --rm --volumes-from s3-backup-test-data-original busybox sh -c 'echo "test data" > /var/s3-backup/test'
+sudo docker run --name s3-backup-test-writer --rm --volumes-from s3-backup-test-data-original busybox sh -c 'echo "test data" > /var/s3-backup/test'
 
 # do a backup
-docker run --rm --name s3-backup-test-backup --volume ~/.boto:/etc/boto.cfg --volumes-from s3-backup-test-data-original digit/s3-backup -s /var/s3-backup -b $BUCKET_NAME -r $REMOTE_PATH
+sudo docker run --rm --name s3-backup-test-backup --volume ~/.boto:/etc/boto.cfg --volumes-from s3-backup-test-data-original trflagg/s3-backup -s /var/s3-backup -b $BUCKET_NAME -r $REMOTE_PATH
 
 # create a new data container
-docker run --name s3-backup-test-data-restore --volume /var/s3-backup busybox true
+sudo docker run --name s3-backup-test-data-restore --volume /var/s3-backup busybox true
 
 # do a restore
-docker run --rm --name s3-backup-test-store --volume ~/.boto:/etc/boto.cfg --volumes-from s3-backup-test-data-restore digit/s3-restore -t /var/s3-backup -b $BUCKET_NAME -r $REMOTE_PATH
+sudo docker run --rm --name s3-backup-test-store --volume ~/.boto:/etc/boto.cfg --volumes-from s3-backup-test-data-restore trflagg/s3-restore -t /var/s3-backup -b $BUCKET_NAME -r $REMOTE_PATH
 
 # check the restore worked
-output=$(docker run --rm --name s3-backup-test-reader --volumes-from s3-backup-test-data-restore busybox sh -c 'cat /var/s3-backup/test')
+output=$(sudo docker run --rm --name s3-backup-test-reader --volumes-from s3-backup-test-data-restore busybox sh -c 'cat /var/s3-backup/test')
 
 if [[ "$output" != "test data" ]]
 then
